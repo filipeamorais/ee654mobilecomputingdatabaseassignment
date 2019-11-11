15633d46f83541634f32997c94755e18218abe38
filipeamorais@pm.me <filipeamorais@pm.me>
2019-11-10 22:46:39 -0600

    add: added the code for the other options of database manipulation

--- app/src/main/java/com/example/databaseassignment/MainActivity.java
+++ app/src/main/java/com/example/databaseassignment/MainActivity.java
@@ -11,7 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = this.findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.database_view);
+        ImageView viewDatabaseImageView = this.findViewById(R.id.viewDatabaseImageView);
+        viewDatabaseImageView.setImageResource(R.drawable.database_view);
+        ImageView editDatabaseImageView = this.findViewById(R.id.editDatabaseImageView);
+        editDatabaseImageView.setImageResource(R.drawable.database_edit);
+        ImageView deleteDatabaseImageView = this.findViewById(R.id.deleteDatabaseImageView);
+        deleteDatabaseImageView.setImageResource(R.drawable.database_delete);
     }
 }
